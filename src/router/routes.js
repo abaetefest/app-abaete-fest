55327e1e1c62114e21df4f9f61138e6c6722c48c
Patrick Monteiro <eng.patrickmonteiro@gmail.com>
2021-10-31 19:20:26 -0300

    fix tabs layout

--- src/router/routes.js
+++ src/router/routes.js
@@ -12,14 +12,14 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: 'home', name: 'home', component: () => import('src/pages/Index.vue') },
-      { path: 'events/:type', name: 'events', component: () => import('src/pages/Events.vue') },
+      { path: 'home', name: 'home', component: () => import('src/pages/Index.vue'), meta: { tab: true } },
+      { path: 'events/:type', name: 'events', component: () => import('src/pages/Events.vue'), meta: { tab: true } },
       { path: 'event-details/:id', name: 'eventDetails', component: () => import('src/pages/EventDetails.vue'), meta: { tab: false } },
       { path: 'manage-events', name: 'manageEvents', component: () => import('src/pages/ManageEvents.vue'), meta: { admin: true } },
-      { path: 'contact', name: 'contact', component: () => import('pages/Contact.vue') },
-      { path: 'combos', name: 'combos', component: () => import('pages/Combos.vue') },
+      { path: 'contact', name: 'contact', component: () => import('pages/Contact.vue'), meta: { tab: true } },
+      { path: 'combos', name: 'combos', component: () => import('pages/Combos.vue'), meta: { tab: true } },
       { path: 'form-events/:id?', name: 'formEvents', component: () => import('pages/FormEvents.vue'), props: true },
-      { path: 'user-informations', name: 'userInformations', component: () => import('pages/UserInformations.vue'), props: true }
+      { path: 'user-informations', name: 'userInformations', component: () => import('pages/UserInformations.vue'), props: true, meta: { tab: true } }
     ],
     meta: {
       requiresAuth: true
