85442e135a23e2008e439a32d61f8d327c570dc0
Patrick Monteiro <eng.patrickmonteiro@gmail.com>
2021-12-03 21:12:03 -0300

    refactor: create menus constants

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -39,6 +39,7 @@
 </template>
 
 <script>
+import { menus } from 'src/constants/menus'
 export default {
   name: 'PageIndex',
   components: {
@@ -46,81 +47,7 @@ export default {
   },
   data () {
     return {
-      opcoes: [
-        {
-          title: 'Festas',
-          icon: 'drink.png',
-          route: 'events',
-          type: 'parties',
-          textColor: 'primary',
-          color: 'white',
-          subtitle: 'As melhores baladas, pubs e barzinhos'
-        },
-        {
-          title: 'Carnaval',
-          icon: 'carnival-mask.png',
-          route: 'events',
-          type: 'carnival',
-          textColor: 'primary',
-          color: 'white',
-          subtitle: 'Programação dos blocos',
-          new: true
-        },
-        {
-          title: 'Cultural',
-          icon: 'guitar.png',
-          route: 'events',
-          type: 'cultural',
-          textColor: 'primary',
-          color: 'white',
-          subtitle: 'Conheça a cultura da cidade'
-        },
-        {
-          title: 'Religiosos',
-          icon: 'church.png',
-          route: 'events',
-          type: 'religious',
-          textColor: 'primary',
-          color: 'white',
-          subtitle: 'Eventos voltados para religiosidadade'
-        },
-        {
-          title: 'Esportivos',
-          icon: 'sport.png',
-          route: 'events',
-          type: 'sports',
-          textColor: 'primary',
-          color: 'white',
-          subtitle: 'Torneios e jogos para você participar'
-        },
-        {
-          title: 'Geek',
-          icon: 'geek.png',
-          route: 'events',
-          type: 'geek',
-          textColor: 'primary',
-          color: 'white',
-          subtitle: 'Games, eventos e muito mais'
-        },
-        {
-          title: 'Automotivo',
-          icon: 'speaker.png',
-          route: 'events',
-          type: 'automotive',
-          textColor: 'primary',
-          color: 'white',
-          subtitle: 'Carro, moto e som automotivo'
-        },
-        {
-          title: 'Turísmo',
-          icon: 'boat.png',
-          route: 'events',
-          type: 'tourism',
-          textColor: 'primary',
-          color: 'white',
-          subtitle: 'Passeios, trilhas e eventos'
-        }
-      ]
+      opcoes: menus
     }
   },
   methods: {
