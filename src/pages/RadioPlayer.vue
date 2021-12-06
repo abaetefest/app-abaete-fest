67594d30912d630fb225c6180647ac960249557b
Patrick Monteiro <eng.patrickmonteiro@gmail.com>
2021-12-06 14:37:07 -0300

    fix: received id for params

--- src/pages/RadioPlayer.vue
+++ src/pages/RadioPlayer.vue
@@ -4,17 +4,20 @@
       <q-icon
         v-if="img"
         :name="`img:radios/${img}`"
-        size="100px"
+        size="130px"
         class="rounded-borders q-pb-md"
       />
     </div>
     <div class="col-xs-12 text-center" v-if="id">
       <audio id="radio-player" controls="" autoplay="" loop="">
         <track kind="captions">
-        <source src="https://9205.brasilstream.com.br/stream.m3u" type="audio/mpeg">
-        <source src="https://9205.brasilstream.com.br/stream" type="audio/mpeg">
+        <source :src="`https://${id}.brasilstream.com.br/stream.m3u`" type="audio/mpeg">
+        <source :src="`https://${id}.brasilstream.com.br/stream`" type="audio/mpeg">
         Seu navegador não suporta o elemento audio
       </audio>
+      <p class="text-caption">
+        Aguarde alguns segundos enquanto o rádio carrega e inicia automáticamente. Caso não inicie aperte no play.
+      </p>
     </div>
   </q-page>
 </template>
