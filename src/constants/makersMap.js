53e2d967c8e392e1fe897f25cea944a93eeb095d
Patrick <eng.patrickmonteiro@gmail.com>
2023-01-20 15:53:19 -0300

    feat: add logos for banks

--- src/constants/makersMap.js
+++ src/constants/makersMap.js
@@ -2,7 +2,7 @@ export const makers = [
   {
     lat: -1.7238821,
     long: -48.8899339,
-    icon: 'flat/bank.png',
+    icon: 'places/bancobrasil.png',
     title: 'Banco do Brasil',
     category: 'Bancos',
     preco: 0,
@@ -10,9 +10,29 @@ export const makers = [
     description: 'Av. Dom Pedro II, 410 - Centro'
   },
   {
+    lat: -1.7259202,
+    long: -48.8800221,
+    icon: 'places/banpara.png',
+    title: 'Banpará',
+    category: 'Bancos',
+    preco: 0,
+    phone: '91 3348-4233',
+    description: 'Av. Dom Pedro II, 1406 - Santa Rosa'
+  },
+  {
+    lat: -1.723824,
+    long: -48.8873937,
+    icon: 'places/basa.png',
+    title: 'Basa',
+    category: 'Bancos',
+    preco: 0,
+    phone: '91 3751-1204',
+    description: 'Av. Dom Pedro II, 270 - Centro'
+  },
+  {
     lat: -1.7237305,
     long: -48.8895201,
-    icon: 'flat/bank.png',
+    icon: 'places/bradesco.png',
     title: 'Bradesco',
     category: 'Bancos',
     preco: 0,
@@ -22,7 +42,7 @@ export const makers = [
   {
     lat: -1.7238821,
     long: -48.8899339,
-    icon: 'flat/bank.png',
+    icon: 'places/caixa.png',
     title: 'Caixa Econômica Federal',
     category: 'Bancos',
     preco: 0,
@@ -32,12 +52,32 @@ export const makers = [
   {
     lat: -1.7242736,
     long: -48.8882101,
-    icon: 'flat/bank.png',
+    icon: 'places/itau.png',
     title: 'Itaú',
     category: 'Bancos',
     preco: 0,
     phone: '91 4004-4828',
     description: 'Rua Barão do Rio Branco, 1506 - Centro'
+  },
+  {
+    lat: -1.7265764,
+    long: -48.89014,
+    icon: 'places/santander.png',
+    title: 'Santander',
+    category: 'Bancos',
+    preco: 0,
+    phone: '0800 702 3535',
+    description: 'Rua Siqueira Mendes, 1208 A - Centro'
+  },
+  {
+    lat: -1.7244284,
+    long: -48.8869443,
+    icon: 'places/sicoob.png',
+    title: 'Sicoob',
+    category: 'Bancos',
+    preco: 0,
+    phone: '91 3258-9443',
+    description: 'Av. Dom Pedro II, 628 - Centro'
   },
   {
     lat: -1.7239596,
