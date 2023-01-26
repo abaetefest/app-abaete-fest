ae46b3330edfa8de46e5f22b957601c744a054fb
Patrick <eng.patrickmonteiro@gmail.com>
2023-01-26 14:39:06 -0300

    feat: add arapari

--- src/constants/trips.js
+++ src/constants/trips.js
@@ -1,4 +1,5 @@
 export const trips = [
+  // Jarumã
   {
     empresa: 'Jarumã Rodofluvial',
     icon: 'trips/jaruma.png',
@@ -146,8 +147,140 @@ export const trips = [
       }
     ]
   },
+  // Arapari Navegação
   {
     empresa: 'Arapari Navegação',
-    icon: 'trips/arapari.png'
+    icon: 'trips/arapari.png',
+    viagens: [
+      {
+        origem: 'Abaetetuba',
+        destino: 'Belém',
+        periodo: 'Segunda a Sábado',
+        horarios: [
+          {
+            hora: '05:30',
+            modalidade: 'Arapari'
+          },
+          {
+            hora: '06:20',
+            modalidade: 'Arapari'
+          },
+          {
+            hora: '07:40',
+            modalidade: 'Arapari'
+          },
+          {
+            hora: '09:45',
+            modalidade: 'Arapari'
+          },
+          {
+            hora: '11:40',
+            modalidade: 'Arapari'
+          },
+          {
+            hora: '15:00',
+            modalidade: 'Arapari'
+          },
+          {
+            hora: '18:45',
+            modalidade: 'Arapari'
+          }
+        ]
+      },
+      {
+        origem: 'Abaetetuba',
+        destino: 'Belém',
+        periodo: 'Domingos e Feriados',
+        horarios: [
+          {
+            hora: '06:45',
+            modalidade: 'Arapari'
+          },
+          {
+            hora: '09:45',
+            modalidade: 'Arapari'
+          },
+          {
+            hora: '11:40',
+            modalidade: 'Arapari'
+          },
+          {
+            hora: '15:00',
+            modalidade: 'Arapari'
+          },
+          {
+            hora: '18:45',
+            modalidade: 'Arapari'
+          }
+        ]
+      },
+      {
+        origem: 'Belém',
+        destino: 'Abaetetuba',
+        periodo: 'Segunda a Sábado',
+        horarios: [
+          {
+            hora: '06:00',
+            modalidade: 'Arapari'
+          },
+          {
+            hora: '06:40',
+            modalidade: 'Arapari'
+          },
+          {
+            hora: '07:40',
+            modalidade: 'Arapari'
+          },
+          {
+            hora: '10:00',
+            modalidade: 'Arapari'
+          },
+          {
+            hora: '12:00',
+            modalidade: 'Arapari'
+          },
+          {
+            hora: '15:00',
+            modalidade: 'Arapari'
+          },
+          {
+            hora: '19:10',
+            modalidade: 'Arapari'
+          },
+          {
+            hora: '23:00',
+            modalidade: 'Arapari',
+            obs: 'Segunda a Sexta'
+          }
+        ]
+      },
+      {
+        origem: 'Belém',
+        destino: 'Abaetetuba',
+        periodo: 'Domingos e Feriados',
+        horarios: [
+          {
+            hora: '07:10',
+            modalidade: 'Arapari'
+          },
+          {
+            hora: '10:00',
+            modalidade: 'Arapari'
+          },
+          {
+            hora: '12:00',
+            modalidade: 'Arapari'
+          },
+          {
+            hora: '15:00',
+            modalidade: 'Arapari'
+          },
+          {
+            hora: '19:10',
+            modalidade: 'Arapari'
+          }
+        ]
+      }
+    ]
   }
 ]
