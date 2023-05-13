d1c654cd34ed826b1199193a34eacb77557cfdc6
Patrick <eng.patrickmonteiro@gmail.com>
2023-05-13 15:30:47 -0300

    feat: add beja

---
+++ src/constants/trips/abaetetuba-beja.js
@@ -0,0 +1,155 @@
+export default {
+  cidade1: 'Abaetetuba',
+  cidade2: 'Beja',
+  empresas: [
+    // Transporte Barreto
+    {
+      nome: 'Diversas Empresas(TransNunes, TransAraujo, etc...)',
+      icon: 'trips/beja.png',
+      viagens: [
+        {
+          origem: 'Abaetetuba',
+          destino: 'Beja',
+          periodo: 'Segunda a Sábado (Saída da Praça da Bandeira, em frente a Pizzaria Sal e Mel.)',
+          horarios: [
+            {
+              hora: '06:30'
+            },
+            {
+              hora: '07:30'
+            },
+            {
+              hora: '08:30'
+            },
+            {
+              hora: '09:30'
+            },
+            {
+              hora: '10:15'
+            },
+            {
+              hora: '11:00'
+            },
+            {
+              hora: '11:45'
+            },
+            {
+              hora: '12:30'
+            },
+            {
+              hora: '13:30'
+            },
+            {
+              hora: '15:00'
+            },
+            {
+              hora: '16:00'
+            },
+            {
+              hora: '17:00'
+            },
+            {
+              hora: '18:00'
+            }
+          ]
+        },
+        {
+          origem: 'Abaetetuba',
+          destino: 'Beja',
+          periodo: 'Domingos e Feriados(Saída da Praça da Bandeira, em frente a Pizzaria Sal e Mel.)',
+          horarios: [
+            {
+              hora: '08:00'
+            },
+            {
+              hora: '09:15'
+            },
+            {
+              hora: '10:30'
+            },
+            {
+              hora: '12:15'
+            },
+            {
+              hora: '16:30'
+            },
+            {
+              hora: '18:00'
+            }
+          ]
+        },
+        {
+          origem: 'Beja',
+          destino: 'Abaetetuba',
+          periodo: 'Segunda a Sábado',
+          horarios: [
+            {
+              hora: '05:30'
+            },
+            {
+              hora: '06:00'
+            },
+            {
+              hora: '06:45'
+            },
+            {
+              hora: '07:30'
+            },
+            {
+              hora: '08:30'
+            },
+            {
+              hora: '09:30'
+            },
+            {
+              hora: '10:30'
+            },
+            {
+              hora: '11:30'
+            },
+            {
+              hora: '12:30'
+            },
+            {
+              hora: '13:30'
+            },
+            {
+              hora: '14:30'
+            },
+            {
+              hora: '16:00'
+            },
+            {
+              hora: '17:00'
+            }
+          ]
+        },
+        {
+          origem: 'Beja',
+          destino: 'Abaetetuba',
+          periodo: 'Domingos e Feriados',
+          horarios: [
+            {
+              hora: '07:00'
+            },
+            {
+              hora: '08:15'
+            },
+            {
+              hora: '09:30'
+            },
+            {
+              hora: '11:00'
+            },
+            {
+              hora: '15:30'
+            },
+            {
+              hora: '17:00'
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
