f6b0c8a3a9b3397e07fae5c44b76c7e7519b694a
Patrick <eng.patrickmonteiro@gmail.com>
2023-08-25 10:56:44 -0300

    feat: add new acai

--- src/constants/acai.js
+++ src/constants/acai.js
@@ -16,7 +16,7 @@ export const acai = [
     title: 'Rota 99',
     category: 'Açaí',
     preco: 0,
-    phone: '91 99128-7751',
+    phone: '91 9 9128-7751',
     address: 'Rua Joaquim Mendes Contente 952, Santa rosa'
   },
   {
@@ -26,7 +26,7 @@ export const acai = [
     title: 'Cuia e Açaí',
     category: 'Açaí',
     preco: 0,
-    phone: '91 99823-0012',
+    phone: '91 9 9823-0012',
     address: 'Dom Pedro I 993, Entre Garibaldi e Manoel Raposo, São João'
   },
   {
@@ -36,7 +36,7 @@ export const acai = [
     title: 'Açaí do Ambró',
     category: 'Açaí',
     preco: 0,
-    phone: '91 99319-8970 | 91 999367-2338',
+    phone: '91 9 9319-8970 | 91 9 99367-2338',
     address: 'Rua Primeiro de Maio 1064, Santa Rosa'
   },
   {
@@ -46,7 +46,27 @@ export const acai = [
     title: 'Estação do Açaí',
     category: 'Açaí',
     preco: 0,
-    phone: '91 98306-2026',
+    phone: '91 9 8306-2026',
     address: 'Avenida São Paulo 1861, Aviação'
+  },
+  {
+    latitude: '-1.7294898',
+    longitude: '-48.8888465',
+    icon: 'flat/acai.png',
+    title: 'Açaí na Tigela',
+    category: 'Açaí',
+    preco: 0,
+    phone: '91 9 89892926',
+    address: 'Travessa Rui Barbosa, n⁰ 573, bairro: Algodoal'
+  },
+  {
+    latitude: '-1.7239057',
+    longitude: '-48.8875346',
+    icon: 'flat/acai.png',
+    title: 'Açaí do centro ',
+    category: 'Açaí',
+    preco: 0,
+    phone: '91 9 92448077',
+    address: 'Av. Dom Pedro II, n⁰ 703- Centro'
   }
 ]
