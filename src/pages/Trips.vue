48bdb2e8a78826e74f75560b4a031fd359f89f6e
Patrick <eng.patrickmonteiro@gmail.com>
2023-08-29 17:11:53 -0300

    fix: remove banner

--- src/pages/Trips.vue
+++ src/pages/Trips.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page padding :class="$q.dark.isActive ? 'bg-dark' :  'bg-grey-1'">
-    <div class="row justify-center lt-md">
+    <div v-if="false" class="row justify-center lt-md">
       <div class="col-xs-12 col-sm-12 col-md-4">
         <q-img src="propagandas/machago.png" @click="openLink" />
       </div>
