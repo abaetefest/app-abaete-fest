d673290819e754d8d0f553c785888d2af21f8120
Waldener Junior <waldener.dev@gmail.com>
2024-01-13 20:10:30 -0300

    refactor: adding churches

---
+++ src/constants/places/igrejas.js
@@ -0,0 +1,164 @@
+export const igrejas = [
+  {
+    latitude: '-1.7230733',
+    longitude: '-48.8881122',
+    icon: 'flat/church.png',
+    title: 'Catedral Nossa Senhora da Conceição',
+    category: 'Igrejas',
+    address: 'R. Barão do Rio Branco - Centro'
+  },
+
+  {
+    latitude: '-1.7166035',
+    longitude: '-48.8890326',
+    icon: 'flat/church.png',
+    title: 'Igreja de São José',
+    category: 'Igrejas',
+    address: 'Tv. José Gonçalves Chaves, 2199 - Cafezal'
+  },
+  {
+    latitude: '-1.7132735',
+    longitude: '-48.8858892',
+    icon: 'flat/church.png',
+    title: 'Igreja de São João Batista',
+    category: 'Igrejas',
+    address: 'R. Barão do Rio Branco, 2798-2830 - São João'
+  },
+  {
+    latitude: '-1.71936335',
+    longitude: '-48.8842651',
+    icon: 'flat/church.png',
+    title: 'Igreja de Santa Rita de Cássia',
+    category: 'Igrejas',
+    address: ' Tv. Aristides dos Réis e Silva, 631 - São Lourenço'
+  },
+  {
+    latitude: '-1.718845',
+    longitude: '-48.882486',
+    icon: 'flat/church.png',
+    title: 'Igreja Nossa Senhora de Nazaré',
+    category: 'Igrejas',
+    address: 'R. Magno de Araújo - São Lourenço'
+  },
+  {
+    latitude: '-1.7190900',
+    longitude: '-48.8771837',
+    icon: 'flat/church.png',
+    title: 'Igreja Sagrado Coração de Jesus',
+    category: 'Igrejas',
+    address: 'R. Raimundo Pauxis, 232-342 - São Lourenço'
+  },
+  {
+    latitude: '-1.7238388',
+    longitude: '-48.8740745',
+    icon: 'flat/church.png',
+    title: 'Igreja do Divino Espírito Santo',
+    category: 'Igrejas',
+    address: 'R. Nova V, 268-2198 - Aviação'
+  },
+  {
+    latitude: '-1.7130254',
+    longitude: '-48.8758395',
+    icon: 'flat/church.png',
+    title: 'Igreja de São Francisco',
+    category: 'Igrejas',
+    address: 'Tv. Paraná, 267-343 - Francilândia'
+  },
+  {
+    latitude: '-1.7080177',
+    longitude: '-48.8744622',
+    icon: 'flat/church.png',
+    title: 'Igreja de São Leopoldo Mandic',
+    category: 'Igrejas',
+    address: 'Tv. Rio Grande do Norte, 189 - Francilândia'
+  },
+  {
+    latitude: '-1.7190421',
+    longitude: '-48.8724868',
+    icon: 'flat/church.png',
+    title: 'Igreja de Santa Maria',
+    category: 'Igrejas',
+    address: 'R. Nova II, 215-125 - Aviação'
+  },
+  {
+    latitude: '-1.7112978',
+    longitude: '-48.8791154',
+    icon: 'flat/church.png',
+    title: 'Igreja de Santa Rita',
+    category: 'Igrejas',
+    address: 'R. Manoel da Silva Raposo, 1086-1346 - São Lourenço'
+  },
+  {
+    latitude: '-1.7194061',
+    longitude: '-48.8641654',
+    icon: 'flat/church.png',
+    title: 'Igreja de São Domingos de Gusmão',
+    category: 'Igrejas',
+    address: 'Av. Anchieta, 2232 - São Domingos da Angélica'
+  },
+  {
+    latitude: '-1.7276912',
+    longitude: '-48.8610978',
+    icon: 'flat/church.png',
+    title: 'Igreja de Santo Antônio',
+    category: 'Igrejas',
+    address: 'R. Dr. João Novaes, 304 - Mutirão'
+  },
+  {
+    latitude: '-1.7284683',
+    longitude: '-48.8554763',
+    icon: 'flat/church.png',
+    title: 'Igreja de Santa Clara de Assis',
+    category: 'Igrejas',
+    address: 'R. Airton Sena, 331 - Santa Clara'
+  },
+  {
+    latitude: '-1.7301263',
+    longitude: '-48.8709174',
+    icon: 'flat/church.png',
+    title: 'Igreja do Cristo Redentor',
+    category: 'Igrejas',
+    address: 'Rod. Dr. João Miranda - Cristo Redentor'
+  },
+  {
+    latitude: '-1.7258835',
+    longitude: '-48.883900',
+    icon: 'flat/church.png',
+    title: 'Igreja de São Pedro',
+    category: 'Igrejas',
+    address: 'Av. Pedro Rodrigues, 886 - Centro'
+  },
+  {
+    latitude: '-1.7266435',
+    longitude: '-48.8862556',
+    icon: 'flat/church.png',
+    title: 'Igreja de São Benedito',
+    category: 'Igrejas',
+    address: 'Tv. Jardim Ataláia, 32-120 - Centro'
+  },
+  {
+    latitude: '-1.7314725',
+    longitude: '-48.8874000',
+    icon: 'flat/church.png',
+    title: 'Igreja de Nossa Senhora do Perpétuo Socorro',
+    category: 'Igrejas',
+    address: 'R. Frei José de Manaus, 603 - Algodoal'
+  },
+  {
+    latitude: '-1.7291998',
+    longitude: '-48.881000',
+    icon: 'flat/church.png',
+    title: 'Igreja de Santa Rosa',
+    category: 'Igrejas',
+    address: 'Tv. Prof. Felipe do Espírito Santo, 203-303 - Santa Rosa'
+  },
+
+  {
+    latitude: '-1.7348492',
+    longitude: '-48.8758009',
+    icon: 'flat/church.png',
+    title: 'Igreja de São Sebastião',
+    category: 'Igrejas',
+    address: 'Tv. São Sebastião, 2307 - São Sebastião'
+  }
+]
