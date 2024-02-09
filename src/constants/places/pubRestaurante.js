37731fa4076b3ebd3471bfcdd7c5b4dd2f4cac21
Waldener Junior <waldener.dev@gmail.com>
2024-02-08 22:59:05 -0300

    refactor: updating lat and pub of pubRestaurante

--- src/constants/places/pubRestaurante.js
+++ src/constants/places/pubRestaurante.js
@@ -1,12 +1,12 @@
 export const pubRestaurante = [
   {
-    latitude: -1.7239596,
-    longitude: -48.8866242,
-    icon: 'map-icon/barbudos.png',
-    title: 'Barbudos',
+    latitude: -1.7240800,
+    longitude: -48.8811225,
+    icon: 'map-icon/casarao.png',
+    title: 'Restaurante e Pizzaria Casarão',
     category: 'Pub&Restaurante',
     preco: 2,
-    address: 'Av. Dom Pedro II, ao lado do INSS.'
+    address: 'R. 7 de Setembro, 1609 - Centro'
   },
   {
     latitude: -1.7245973,
@@ -18,13 +18,13 @@ export const pubRestaurante = [
     address: 'Av. Dom Pedro II, 1079'
   },
   {
-    latitude: -1.724212,
-    longitude: -48.884634,
-    icon: 'map-icon/lenhador.png',
-    title: 'Lenhador',
+    latitude: -1.724571,
+    longitude: -48.8821012,
+    icon: 'map-icon/simple.png',
+    title: 'Simple. Bar & Restaurante',
     category: 'Pub&Restaurante',
     preco: 2,
-    address: 'Av. Dom Pedro II, 742'
+    address: 'Av. Dom Pedro II, 1035 - Centro'
   },
   {
     latitude: -1.7243967,
@@ -36,6 +36,15 @@ export const pubRestaurante = [
     address: 'Avenida Dom Pedro II'
   },
   {
+    latitude: -1.724292,
+    longitude: -48.884684,
+    icon: 'map-icon/lenhador.png',
+    title: 'Lenhador',
+    category: 'Pub&Restaurante',
+    preco: 2,
+    address: 'Av. Dom Pedro II, 742'
+  },
+  {
     latitude: -1.7208352,
     longitude: -48.8836972,
     icon: 'map-icon/texas.png',
@@ -45,22 +54,40 @@ export const pubRestaurante = [
     address: 'R. Magno de Araújo, 1959 - São Lourenço'
   },
   {
-    latitude: -1.7238736,
-    longitude: -48.8867696,
-    icon: 'map-icon/ducaliari.png',
-    title: "Du'Calliari Pub & Restô",
-    category: 'Pub&Restaurante',
-    preco: 2,
-    address: 'Av. Dom Pedro II, 502 - Centro'
-  },
-  {
     latitude: -1.7238808,
-    longitude: -48.8851456,
+    longitude: -48.8852956,
     icon: 'map-icon/domo.png',
     title: 'Restaurante DOMO',
     category: 'Pub&Restaurante',
     preco: 3,
     address: 'Av. Dom Pedro II, 681 - Centro'
+  },
+  {
+    latitude: -1.7242825,
+    longitude: -48.8848977,
+    icon: 'map-icon/spazio_gourmet.png',
+    title: 'Spazio Gourmet - Burger & Pizza',
+    category: 'Pub&Restaurante',
+    preco: 2,
+    address: 'Av. Dom Pedro II, 712 - Centro'
+  },
+  {
+    latitude: -1.7240142,
+    longitude: -48.8860800,
+    icon: 'map-icon/salemelgrelhados.png',
+    title: 'Sal e Mel Grelhados',
+    category: 'Pub&Restaurante',
+    preco: 2,
+    address: 'Av. Dom Pedro II, 696 - Centro'
+  },
+  {
+    latitude: -1.7238736,
+    longitude: -48.8868696,
+    icon: 'map-icon/ducaliari.png',
+    title: "Du'Calliari Pub & Restô",
+    category: 'Pub&Restaurante',
+    preco: 2,
+    address: 'Av. Dom Pedro II, 502 - Centro'
   },
   {
     latitude: -1.7274963,
@@ -72,17 +99,8 @@ export const pubRestaurante = [
     address: 'Rod. Dr. João Miranda, 1724 - Santa Rosa'
   },
   {
-    latitude: -1.724571,
-    longitude: -48.8821012,
-    icon: 'map-icon/simple.png',
-    title: 'Simple. Bar & Restaurante',
-    category: 'Pub&Restaurante',
-    preco: 2,
-    address: 'Av. Dom Pedro II, 1035 - Centro'
-  },
-  {
-    latitude: -1.7284656,
-    longitude: -48.8943648,
+    latitude: -1.7284956,
+    longitude: -48.8942448,
     icon: 'map-icon/malocabelavista.png',
     title: 'Maloca Bela Vista',
     category: 'Pub&Restaurante',
@@ -99,8 +117,8 @@ export const pubRestaurante = [
     address: 'R. Siqueira Mendes, 2207 - São José'
   },
   {
-    latitude: -1.7144146,
-    longitude: -48.9105237,
+    latitude: -1.7112146,
+    longitude: -48.9093037,
     icon: 'map-icon/preamar.png',
     title: 'Bar Point Pré a Mar',
     category: 'Pub&Restaurante',
@@ -108,7 +126,7 @@ export const pubRestaurante = [
     address: 'Rio Sirituba - Abaetetuba'
   },
   {
-    latitude: -1.7171225,
+    latitude: -1.7170225,
     longitude: -48.8785428,
     icon: 'map-icon/cantinhonilda.png',
     title: 'Cantinho da Tia Nilda',
@@ -127,39 +145,12 @@ export const pubRestaurante = [
   },
   {
     latitude: -1.7129316,
-    longitude: -48.887089,
+    longitude: -48.886889,
     icon: 'map-icon/paxiuba.png',
     title: 'Bar E Restaurante Paxiuba',
     category: 'Pub&Restaurante',
     preco: 2,
     address: 'Tv. São Joaquim, 1-117 - São João'
-  },
-  {
-    latitude: -1.7241297,
-    longitude: -48.8811825,
-    icon: 'map-icon/casarao.png',
-    title: 'Restaurante e Pizzaria Casarão',
-    category: 'Pub&Restaurante',
-    preco: 2,
-    address: 'R. 7 de Setembro, 1609 - Centro'
-  },
-  {
-    latitude: -1.7242825,
-    longitude: -48.8847577,
-    icon: 'map-icon/spazio_gourmet.png',
-    title: 'Spazio Gourmet - Burger & Pizza',
-    category: 'Pub&Restaurante',
-    preco: 2,
-    address: 'Av. Dom Pedro II, 712 - Centro'
-  },
-  {
-    latitude: -1.7239942,
-    longitude: -48.8860279,
-    icon: 'map-icon/salemelgrelhados.png',
-    title: 'Sal e Mel Grelhados',
-    category: 'Pub&Restaurante',
-    preco: 2,
-    address: 'Av. Dom Pedro II, 696 - Centro'
   },
   {
     latitude: -1.7257391,
@@ -172,7 +163,7 @@ export const pubRestaurante = [
   },
   {
     latitude: -1.7199928,
-    longitude: -48.8833546,
+    longitude: -48.8834146,
     icon: 'map-icon/divinacolher.png',
     title: 'Divina Colher',
     category: 'Pub&Restaurante',
