d62afb68a52a1ef761f3a9b5e73f1cd3339ef77e
Waldener Junior <waldener.dev@gmail.com>
2025-07-04 14:11:45 -0300

    feat: add summer 2025 trip schedule for Abaetetuba to Beja
    - Introduced a new export for the summer 2025 trip schedule, detailing routes, periods, and timings for both directions between Abaetetuba and Beja.
    - Reformatted existing period strings for consistency and improved readability.

--- src/constants/trips/abaetetuba-beja.js
+++ src/constants/trips/abaetetuba-beja.js
@@ -10,7 +10,8 @@ export default {
         {
           origem: 'Abaetetuba',
           destino: 'Beja',
-          periodo: 'Segunda a Sábado (Saída da Praça da Bandeira, em frente a Pizzaria Sal e Mel.)',
+          periodo:
+            'Segunda a Sábado (Saída da Praça da Bandeira, em frente a Pizzaria Sal e Mel.)',
           horarios: [
             {
               hora: '06:30'
@@ -56,7 +57,8 @@ export default {
         {
           origem: 'Abaetetuba',
           destino: 'Beja',
-          periodo: 'Domingos e Feriados(Saída da Praça da Bandeira, em frente a Pizzaria Sal e Mel.)',
+          periodo:
+            'Domingos e Feriados(Saída da Praça da Bandeira, em frente a Pizzaria Sal e Mel.)',
           horarios: [
             {
               hora: '08:00'
@@ -152,4 +154,263 @@ export default {
       ]
     }
   ]
+}
+
+export const bejaVerao2025 = {
+  cidade1: 'Abaetetuba',
+  cidade2: 'Beja',
+  empresa: {
+    nome: 'Viação Beja Express',
+    icon: '/public/trips/expresso.png',
+    viagens: [
+      {
+        origem: 'Abaetetuba',
+        destino: 'Beja',
+        periodo: 'Sexta-feira',
+        horarios: [
+          {
+            hora: '06:15',
+            modalidade: 'Pç. da Bandeira'
+          },
+          {
+            hora: '07:30',
+            modalidade: 'Pç. da Bandeira'
+          },
+          {
+            hora: '08:30',
+            modalidade: 'Pç. da Bandeira'
+          },
+          {
+            hora: '09:30',
+            modalidade: 'Pç. da Bandeira'
+          },
+          {
+            hora: '10:15',
+            modalidade: 'Pç. da Bandeira'
+          },
+          {
+            hora: '11:00',
+            modalidade: 'Pç. da Bandeira'
+          },
+          {
+            hora: '11:45',
+            modalidade: 'Pç. da Bandeira'
+          },
+          {
+            hora: '12:30',
+            modalidade: 'Pç. da Bandeira'
+          },
+          {
+            hora: '13:30',
+            modalidade: 'Pç. da Bandeira'
+          },
+          {
+            hora: '14:30',
+            modalidade: 'Pç. da Bandeira'
+          },
+          {
+            hora: '15:30',
+            modalidade: 'Pç. da Bandeira'
+          },
+          {
+            hora: '16:15',
+            modalidade: 'Pç. da Bandeira'
+          },
+          {
+            hora: '17:00',
+            modalidade: 'Pç. da Bandeira'
+          },
+          {
+            hora: '18:00',
+            modalidade: 'Pç. da Bandeira'
+          },
+          {
+            hora: '19:00',
+            modalidade: 'Ter. Rodoviário'
+          },
+          {
+            hora: '19:30',
+            modalidade: 'Ter. Rodoviário'
+          },
+          {
+            hora: '20:00',
+            modalidade: 'Ter. Rodoviário'
+          },
+          {
+            hora: '20:30',
+            modalidade: 'Ter. Rodoviário'
+          },
+          {
+            hora: '21:00',
+            modalidade: 'Ter. Rodoviário'
+          },
+          {
+            hora: '21:30',
+            modalidade: 'Ter. Rodoviário'
+          },
+          {
+            hora: '22:00',
+            modalidade: 'Ter. Rodoviário'
+          },
+          {
+            hora: '22:30',
+            modalidade: 'Ter. Rodoviário'
+          },
+          {
+            hora: '23:00',
+            modalidade: 'Ter. Rodoviário'
+          }
+        ]
+      },
+      {
+        origem: 'Abaetetuba',
+        destino: 'Beja',
+        periodo: 'Sábado (manhã)',
+        horarios: [
+          {
+            hora: '06:15',
+            modalidade: 'Pç. da Bandeira'
+          },
+          {
+            hora: '07:15',
+            modalidade: 'Pç. da Bandeira'
+          },
+          {
+            hora: '08:15',
+            modalidade: 'Pç. da Bandeira'
+          },
+          {
+            hora: '09:00',
+            modalidade: 'Pç. da Bandeira'
+          },
+          {
+            hora: '09:40',
+            modalidade: 'Pç. da Bandeira'
+          },
+          {
+            hora: '10:30',
+            modalidade: 'Pç. da Bandeira'
+          },
+          {
+            hora: '11:10',
+            modalidade: 'Pç. da Bandeira'
+          },
+          {
+            hora: '12:00',
+            modalidade: 'Pç. da Bandeira'
+          }
+        ]
+      },
+      {
+        origem: 'Beja',
+        destino: 'Abaetetuba',
+        periodo: 'Sexta-feira',
+        horarios: [
+          {
+            hora: '06:00',
+            modalidade: 'Beja'
+          },
+          {
+            hora: '06:45',
+            modalidade: 'Beja'
+          },
+          {
+            hora: '07:30',
+            modalidade: 'Beja'
+          },
+          {
+            hora: '08:30',
+            modalidade: 'Beja'
+          },
+          {
+            hora: '09:30',
+            modalidade: 'Beja'
+          },
+          {
+            hora: '10:30',
+            modalidade: 'Beja'
+          },
+          {
+            hora: '11:30',
+            modalidade: 'Beja'
+          },
+          {
+            hora: '12:30',
+            modalidade: 'Beja'
+          },
+          {
+            hora: '13:30',
+            modalidade: 'Beja'
+          },
+          {
+            hora: '14:30',
+            modalidade: 'Beja'
+          },
+          {
+            hora: '15:15',
+            modalidade: 'Beja'
+          },
+          {
+            hora: '16:00',
+            modalidade: 'Beja'
+          },
+          {
+            hora: '17:00',
+            modalidade: 'Beja'
+          },
+          {
+            hora: '18:00',
+            modalidade: 'Beja'
+          }
+        ]
+      },
+      {
+        origem: 'Beja',
+        destino: 'Abaetetuba',
+        periodo: 'Sábado (manhã)',
+        horarios: [
+          {
+            hora: '05:15',
+            modalidade: 'Beja'
+          },
+          {
+            hora: '06:00',
+            modalidade: 'Beja'
+          },
+          {
+            hora: '06:30',
+            modalidade: 'Beja'
+          },
+          {
+            hora: '07:00',
+            modalidade: 'Beja'
+          },
+          {
+            hora: '07:30',
+            modalidade: 'Beja'
+          },
+          {
+            hora: '08:30',
+            modalidade: 'Beja'
+          },
+          {
+            hora: '09:30',
+            modalidade: 'Beja'
+          },
+          {
+            hora: '10:30',
+            modalidade: 'Beja'
+          },
+          {
+            hora: '11:30',
+            modalidade: 'Beja'
+          },
+          {
+            hora: '12:30',
+            modalidade: 'Beja'
+          }
+        ]
+      }
+    ]
+  }
 }
