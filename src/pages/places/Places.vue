88dc95667bf26e44246e82508d5c9d78d19c7f14
Waldener Junior <waldener.dev@gmail.com>
2025-07-07 15:34:30 -0300

    feat: add Places page with dynamic category selection and modal details
    - Created Places.vue component for displaying a list of places with category filtering.
    - Implemented places.js for managing data and methods related to places.
    - Added DialogPlaces.vue component for showing detailed information about selected places in a modal.
    - Enhanced user experience with responsive design and improved interaction through category selection and modal dialogs.

--- src/pages/places/Places.vue
+++ src/pages/places/Places.vue
@@ -110,48 +110,4 @@
   </q-page>
 </template>
 
-<script>
-import { makers } from 'src/constants/places/index'
-import { placesCategories } from 'src/constants/placesCategories'
-
-export default {
-  name: 'PlacesPage',
-  components: {
-    DialogPlaces: () => import('components/DialogPlaces')
-  },
-  data() {
-    return {
-      places: makers,
-      ratingModel: 3,
-      categoria: 'Açaí',
-      options: [...placesCategories],
-      modalPlaces: false,
-      placeDetails: {}
-    }
-  },
-  computed: {
-    getPlaces: function () {
-      return makers.filter((place) => place.category === this.categoria)
-    },
-    getIconCategory: function () {
-      const img = this.options.filter((opt) => opt.value === this.categoria)
-      return img[0].icon
-    }
-  },
-  mounted() {
-    console.log(this.categoria)
-  },
-  methods: {
-    openDialogCourse(place) {
-      this.modalPlaces = true
-      this.placeDetails = place
-      this.$mixpanel.track(place.title)
-    },
-    closeModal() {
-      this.modalPlaces = false
-      this.placeDetails = {}
-    }
-  }
-}
-</script>
-src/constants/placesCategories
+<script src="./places.js"></script>
