1a462dad5d627eae6b8b1aac356a47da71f0a866
Waldener Junior <waldener.dev@gmail.com>
2025-10-19 20:26:48 -0300

    feat: update event categories in category.js
    - Replaced 'Religiosos' with 'Saúde' and added 'Turismo' to the event categories for better representation.
    - Restored 'Religiosos' category to maintain existing structure after reordering.

--- src/constants/category.js
+++ src/constants/category.js
@@ -3,9 +3,10 @@ export const category = [
   { label: 'Festas', value: 'parties', icon: 'mdi-glass-mug-variant' },
   { label: 'Carnaval', value: 'carnival', icon: 'mdi-drama-masks' },
   { label: 'Cultural', value: 'cultural', icon: 'mdi-guitar-acoustic' },
-  { label: 'Religiosos', value: 'religious', icon: 'mdi-church' },
-  { label: 'Esportivos', value: 'sports', icon: 'mdi-soccer' },
-  { label: 'Geek', value: 'geek', icon: 'mdi-wizard-hat' },
+  { label: 'Saúde', value: 'health', icon: 'mdi-heart-pulse' },
   { label: 'Turismo', value: 'tourism', icon: 'mdi-sail-boat' },
+  { label: 'Esportivos', value: 'sports', icon: 'mdi-soccer' },
+  { label: 'Religiosos', value: 'religious', icon: 'mdi-church' },
+  { label: 'Geek', value: 'geek', icon: 'mdi-wizard-hat' },
   { label: 'Automotivo', value: 'automotive', icon: 'mdi-car-sports' }
 ]
