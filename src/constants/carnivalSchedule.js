1e6390eed7a8f2a71e8a2dd8e2c9e30f89d5f35c
Waldener Junior <waldener.dev@gmail.com>
2026-02-17 16:10:23 -0300

    feat: add new carnival event image and update Tuesday schedule details

--- src/constants/carnivalSchedule.js
+++ src/constants/carnivalSchedule.js
@@ -4,26 +4,15 @@
  */
 export const CARNIVAL_SCHEDULE_EVENTS = [
   {
-    id: 'carnival-4',
-    dia_semana: 'Segunda-Feira',
-    name: 'Baile De Máscaras',
-    start_date: '2025-02-16T16:00:00',
-    location: 'Avenida Principal, Abaetetuba',
-    image_url: '/carnaval/programacao-carnaval-4.1.jpg',
-    category: 'carnival',
-    description:
-      '<p><b>A SEGUNDA-FEIRA MAIS AGITADA DO ANO CHEGOU!</b> 🥁🔥</p><p>No dia 16 de fevereiro, o Carnaval de Abaetetuba continua com força total e uma programação que vai fazer história. Tem blocos tradicionais, pagode, sofrência e o melhor do axé para ninguém ficar parado! Confira o cronograma:</p><ul><li><p>16h – Bloco Kanto Nu Xadrez com Styllus Show</p></li><li><p>19h – Bloco Pikasso</p></li><li><p>19h30 – Bloco Exclusive</p></li><li><p>20h – DJ Rodrigo Moraes</p></li><li><p>20h30 – NOSSO TOM</p></li><li><p>21h – Bloco Lobos na Folia</p></li><li><p>21h30 – DJ Bilo Batidão</p></li><li><p>22h – Bloco Pegou Pegou</p></li><li><p>22h30 – Allanzinho</p></li><li><p>23h – MARÍLIA TAVARES</p></li><li><p>23h30 – Bloco Tubarão Evolution</p></li><li><p>00h – DJ Adê</p></li><li><p>01h – FRUTO SENSUAL</p></li><li><p>02h – Zaynara</p></li></ul><p>Prepare sua energia, chame os amigos e venha para a avenida viver essa emoção! 🚀✨</p><p><b>#Carnaval2026 #Abaetetuba #ChicleteComBanana #MaríliaTavares #NossoTom #FrutoSensual #Folia #SegundaDeCarnaval</b></p>'
-  },
-  {
     id: 'carnival-5',
     dia_semana: 'Terça-Feira',
     name: 'Queima Do Maracatu',
     start_date: '2025-02-17T16:00:00',
     location: 'Avenida Principal, Abaetetuba',
-    image_url: '/carnaval/programacao-carnaval-5.jpg',
+    image_url: '/carnaval/programacao-carnaval-5.1.jpg',
     category: 'carnival',
     description:
-      '<p><b>🎊 TERÇA-FEIRA DE FOLIA: CARNAVAL DE ABAETETUBA (17 FEV)</b></p><p>Confira a programação oficial para esta terça-feira de folia:</p><ul><li><p>16h – Bloco das Virgens com Rodriguinho e Lolla</p></li><li><p>19h00 – Bloco Leões na folia</p></li><li><p>19h30 – Bloco GD8 na Folia</p></li><li><p>20h – DJ Gigio Boy</p></li><li><p>20h30 – Bloco Carna Spa na Folia</p></li><li><p>20h45 – Bloco Cuspe no Garoto</p></li><li><p>21h – Bloco Rock das Equipes</p></li><li><p>21h30 – Beto Farias e Banda</p></li><li><p>22h – DJ Cesar Considerado</p></li><li><p>23h – Ju Márques</p></li><li><p>23h30 – DJ Gulliver</p></li><li><p>00h – FB Mania</p></li><li><p>01h – DJ Adriel da Mídia</p></li><li><p>02h – Gaby Amarantos</p></li></ul><p>Prepare a sua fantasia e venha viver cada segundo dessa folia histórica! 🚀🔥</p><p><b>#Carnaval2026 #Abaetetuba #GabyAmarantos #BlocoDasVirgens #Folia #TerçaDeCarnaval #ProgramaçãoCarnaval</b></p>'
+      '<p><b>PARA NÃO SE PERDER NA FOLIA: CONFIRA O HORÁRIO DE CADA ATRAÇÃO!</b> 📅🎊</p><p>O Carnaval de Abaetetuba preparou uma terça-feira intensa, do Bloco das Virgens até o amanhecer com Ju Márques. Salve este cronograma oficial e compartilhe com a sua galera!</p><p><b>🕒 PROGRAMAÇÃO EM ORDEM:</b></p><ul><li><p>16h00: Bloco das Virgens com Rodriguinho e Lolla</p></li><li><p>18h30: Reinaldinho (Pipoca da PMA)</p></li><li><p>19h00: Bloco Leões na Folia</p></li><li><p>19h30: Bloco GD8 na Folia</p></li><li><p>20h00: DJ Gigio Boy (Pipoca da PMA)</p></li><li><p>20h30: Bloco Carna Spa na Folia</p></li><li><p>20h45: Bloco Cuspe no Garoto</p></li><li><p>21h00: Bloco Rock das Equipes</p></li><li><p>21h30: Beto Farias e Banda (Pipoca da PMA)</p></li><li><p>22h00: Gaby Amarantos (Pipoca da PMA)</p></li><li><p>23h00: DJ Cesar Considerado (DJ na Folia)</p></li><li><p>23h30: DJ Gulliver (DJ na Folia)</p></li><li><p>00h00: FB Mania (Pipoca da PMA)</p></li><li><p>01h00: DJ Adriel da Mídia (DJ na Folia)</p></li><li><p>02h00: Ju Márques (Pipoca da PMA)</p></li></ul><p>Nos vemos no corredor da folia! 🚀✨</p><p><b>#CarnavalDeAbaetetuba #CronogramaCarnaval #GabyAmarantos #Abaetetuba #Carnaval2026 #Folia #Pará</b></p>'
   },
   {
     id: 'carnival-6',
